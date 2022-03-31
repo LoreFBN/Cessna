440bb890e31d76ba31d76c40aadbbba3cf7e15d7
LoreFBN <102668240+LoreFBN@users.noreply.github.com>
2022-03-31 09:50:27 +0200

    Modified modus operandi
    file input from keyboard
    read_engine passing address so if the filename had a typo, it will be fixed.
    added the function write_engine, but its a work in progress

--- main.c
+++ main.c
@@ -5,9 +5,13 @@
 #include "libs/engine/engine_rwv.h"
 
 int main(){
-    char filename[20]= "engine";
-    //printf("inserire il nome del file di testo:");
-    //scanf("%s",filename);
-    read_engine(strcat(filename,".txt"));
+    char filename[20];
+    printf("Type the text file: ");
+    scanf("%s",filename);
+    //printf("You typed: %s\n",strcat(filename,".txt"));
+    read_engine(&filename[0]);
+    //printf("correct filename updated in the main: %s\n",filename);
+    db engine = write_engine(filename);
+    //printf("you modified engine as: %f",engine.P_max_sl);
     return 0;
 }
